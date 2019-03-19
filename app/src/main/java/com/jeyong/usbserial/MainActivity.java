2bc5e48e0257becb65371048ae589b71e26d07dc
jeyong <jeyong@gmail.com>
2019-03-19 10:52:45 +0900

    add gradle configuration and usbservice.

--- app/src/main/java/com/jeyong/usbserial/MainActivity.java
+++ app/src/main/java/com/jeyong/usbserial/MainActivity.java
@@ -1,7 +1,15 @@
 package com.jeyong.usbserial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import io.dronefleet.mavlink.MavlinkConnection;
+import io.dronefleet.mavlink.MavlinkMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, UsbService.class);
+        startService(intent);
+    }
+
+    public void getIn(){
+        InputStream in = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 0;
+            }
+        };
+
+        OutputStream out = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                return ;
+            }
+        };
+
+        MavlinkConnection connection = MavlinkConnection.create(in, out);
+        MavlinkMessage message;
+
+        try {
+            message = connection.next();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
